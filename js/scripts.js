6f1a00f40c3a08eb4523822d0b4fc8d89a4f037e
sabri <sabri.rhaimia@gmail.com>
2018-03-22 20:43:06 +0100

    init repo with comming soon template

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // -------------------------------------------------------------
+    // Countdown
+    // -------------------------------------------------------------
+    (function () {
+
+        $("#back-countdiown").countdown({
+            date: "15 may 2018 20:00:00",
+            format: "on"
+        });
+    
+    }()); 
+
+
+}); // JQuery end
+
+
+$(document).on('click', '.m-menu .dropdown-menu', function(e) {
+  e.stopPropagation()
+})
