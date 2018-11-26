d08f871f5b38868e9a84a303990f8d54e99fa094
Sabri <sabri.rhaimia@gmail.com>
2018-11-26 22:58:43 +0100

    WIP: integrate fullpage.js && fix slider && remove extra scripts

--- js/fullpageconfig.js
+++ js/fullpageconfig.js
@@ -1,10 +1,70 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-		scrollHorizontally: true
-	});
+			//Navigation
+	menu: 'nav.menu',
+	navigation: false,
+	navigationPosition: 'right',
+	showActiveTooltip: false,
+	slidesNavigation: false,
 
-	//methods
-	$.fn.fullpage.setAllowScrolling(false);
+	//Scrolling
+	css3: true,
+	scrollingSpeed: 700,
+	autoScrolling: true,
+	fitToSection: true,
+	fitToSectionDelay: 1000,
+	scrollBar: false,
+	easing: 'easeInOutCubic',
+	easingcss3: 'ease',
+	loopBottom: false,
+	loopTop: false,
+	loopHorizontal: true,
+	continuousVertical: false,
+	continuousHorizontal: false,
+	scrollHorizontally: false,
+	interlockedSlides: false,
+	dragAndMove: false,
+	offsetSections: false,
+	resetSliders: false,
+	fadingEffect: false,
+	normalScrollElements: '#element1, .element2',
+	scrollOverflow: false,
+	scrollOverflowReset: false,
+	scrollOverflowOptions: null,
+	touchSensitivity: 15,
+	normalScrollElementTouchThreshold: 5,
+	bigSectionsDestination: null,
+
+	//Accessibility
+	keyboardScrolling: true,
+	animateAnchor: true,
+	recordHistory: true,
+
+	//Design
+	controlArrows: false,
+	verticalCentered: true,
+	paddingTop: '3em',
+	paddingBottom: '10px',
+	fixedElements: '#header, .footer',
+	responsiveWidth: 0,
+	responsiveHeight: 0,
+	responsiveSlides: false,
+	parallax: false,
+	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+	//Custom selectors
+	sectionSelector: '.section',
+	slideSelector: '.slide',
+
+	lazyLoading: true,
+
+	//events
+	onLeave: function(origin, destination, direction){},
+	afterLoad: function(origin, destination, direction){},
+	afterRender: function(){},
+	afterResize: function(width, height){},
+	afterResponsive: function(isResponsive){},
+	afterSlideLoad: function(section, origin, destination, direction){},
+	onSlideLeave: function(section, origin, destination, direction){}
+	});
 });
--- js/main.js
+++ js/main.js
@@ -8,33 +8,33 @@
  * Copyright 2015, Codrops
  * http://www.codrops.com
  */
-;(function(window) {
+; (function (window) {
 
 	'use strict';
 
-	var bodyEl = document.body, 
+	var bodyEl = document.body,
 		docElem = window.document.documentElement,
 		support = { transitions: Modernizr.csstransitions },
 		// transition end event name
 		transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		onEndTransition = function( el, callback ) {
-			var onEndCallbackFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.target != this ) return;
-					this.removeEventListener( transEndEventName, onEndCallbackFn );
+		transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+		onEndTransition = function (el, callback) {
+			var onEndCallbackFn = function (ev) {
+				if (support.transitions) {
+					if (ev.target != this) return;
+					this.removeEventListener(transEndEventName, onEndCallbackFn);
 				}
-				if( callback && typeof callback === 'function' ) { callback.call(this); }
+				if (callback && typeof callback === 'function') { callback.call(this); }
 			};
-			if( support.transitions ) {
-				el.addEventListener( transEndEventName, onEndCallbackFn );
+			if (support.transitions) {
+				el.addEventListener(transEndEventName, onEndCallbackFn);
 			}
 			else {
 				onEndCallbackFn();
 			}
 		},
 		// window sizes
-		win = {width: window.innerWidth, height: window.innerHeight},
+		win = { width: window.innerWidth, height: window.innerHeight },
 		// some helper vars to disallow scrolling
 		lockScroll = false, xscroll, yscroll,
 		scrollContainer = document.querySelector('#container'),
@@ -50,7 +50,8 @@
 		// the main content element
 		contentEl = document.querySelector('.content'),
 		// close content control
-		closeContentCtrl = contentEl.querySelector('button.button--close'),
+		closeContentCtrl = $('button.button--close')[0],
+
 		// index of current item
 		current = 0,
 		// check if an item is "open"
@@ -59,6 +60,8 @@
 		// scale body when zooming into the items, if not Firefox (the performance in Firefox is not very good)
 		bodyScale = isFirefox ? false : 3;
 
+	console.log('close content ctrl :: ', closeContentCtrl);
+
 	// some helper functions:
 	function scrollX() { return window.pageXOffset || docElem.scrollLeft; }
 	function scrollY() { return window.pageYOffset || docElem.scrollTop; }
@@ -66,10 +69,10 @@
 	function throttle(fn, delay) {
 		var allowSample = true;
 
-		return function(e) {
+		return function (e) {
 			if (allowSample) {
 				allowSample = false;
-				setTimeout(function() { allowSample = true; }, delay);
+				setTimeout(function () { allowSample = true; }, delay);
 				fn(e);
 			}
 		};
@@ -82,7 +85,7 @@
 	// event binding
 	function initEvents() {
 		// open items
-		zoomCtrl.addEventListener('click', function() {
+		zoomCtrl.addEventListener('click', function () {
 			openItem(items[current]);
 		});
 
@@ -90,25 +93,25 @@
 		closeContentCtrl.addEventListener('click', closeContent);
 
 		// navigation
-		navRightCtrl.addEventListener('click', function() { navigate('right'); });
-		navLeftCtrl.addEventListener('click', function() { navigate('left'); });
+		navRightCtrl.addEventListener('click', function () { navigate('right'); });
+		navLeftCtrl.addEventListener('click', function () { navigate('left'); });
 
 		// window resize
-		window.addEventListener('resize', throttle(function(ev) {
+		window.addEventListener('resize', throttle(function (ev) {
 			// reset window sizes
-			win = {width: window.innerWidth, height: window.innerHeight};
+			win = { width: window.innerWidth, height: window.innerHeight };
 
 			// reset transforms for the items (slider items)
-			items.forEach(function(item, pos) {
-				if( pos === current ) return;
+			items.forEach(function (item, pos) {
+				if (pos === current) return;
 				var el = item.querySelector('.slide__mover');
 				dynamics.css(el, { translateX: el.offsetWidth });
 			});
 		}, 10));
 
 		// keyboard navigation events
-		document.addEventListener( 'keydown', function( ev ) {
-			if( isOpen ) return; 
+		document.addEventListener('keydown', function (ev) {
+			if (isOpen) return;
 			var keyCode = ev.keyCode || ev.which;
 			switch (keyCode) {
 				case 37:
@@ -118,33 +121,38 @@
 					navigate('right');
 					break;
 			}
-		} );
+		});
 	}
 
 	// opens one item
 	function openItem(item) {
-		if( isOpen ) return;
+		if (isOpen) return;
 		isOpen = true;
 
 		// the element that will be transformed
 		var zoomer = item.querySelector('.zoomer');
 		// slide screen preview
 		classie.add(zoomer, 'zoomer--active');
+		// classie.add($('#description-content'), 'active');
+		$('#description-content').addClass('active');
 		// disallow scroll
 		scrollContainer.addEventListener('scroll', noscroll);
 		// apply transforms
 		applyTransforms(zoomer);
 		// also scale the body so it looks the camera moves to the item.
-		if( bodyScale ) {
+		if (bodyScale) {
 			dynamics.animate(bodyEl, { scale: bodyScale }, { type: dynamics.easeInOut, duration: 500 });
 		}
 		// after the transition is finished:
-		onEndTransition(zoomer, function() {
+		onEndTransition(zoomer, function () {
+			console.log('end transition !!');
 			// reset body transform
-			if( bodyScale ) {
+			if (bodyScale) {
+				console.log('body scale true !!');
+
 				dynamics.stop(bodyEl);
 				dynamics.css(bodyEl, { scale: 1 });
-				
+
 				// fix for safari (allowing fixed children to keep position)
 				bodyEl.style.WebkitTransform = 'none';
 				bodyEl.style.transform = 'none';
@@ -152,7 +160,9 @@
 			// no scrolling
 			classie.add(bodyEl, 'noscroll');
 			classie.add(contentEl, 'content--open');
-			var contentItem = document.getElementById(item.getAttribute('data-content'))
+			var contentItem = document.getElementById(item.getAttribute('data-content'));
+			console.log('contentItem !!', contentItem);
+			console.log('zoomer !!', zoomer);
 			classie.add(contentItem, 'content__item--current');
 			classie.add(contentItem, 'content__item--reset');
 
@@ -166,14 +176,17 @@
 
 	// closes the item/content
 	function closeContent() {
-		var contentItem = contentEl.querySelector('.content__item--current'),
+		var contentItem = $('.content__item--current'),
 			zoomer = items[current].querySelector('.zoomer');
+
+		// classie.remove($('#description-content'), 'active');
+		$('#description-content').removeClass('active');
 
 		classie.remove(contentEl, 'content--open');
 		classie.remove(contentItem, 'content__item--current');
 		classie.remove(bodyEl, 'noscroll');
-				
-		if( bodyScale ) {
+
+		if (bodyScale) {
 			// reset fix for safari (allowing fixed children to keep position)
 			bodyEl.style.WebkitTransform = '';
 			bodyEl.style.transform = '';
@@ -185,9 +198,9 @@
 		/* fix for safari flickering */
 
 		// wait for the inner content to finish the transition
-		onEndTransition(contentItem, function(ev) {
+		onEndTransition(contentItem, function (ev) {
 			classie.remove(this, 'content__item--reset');
-			
+
 			// reset scrolling permission
 			lockScroll = false;
 			scrollContainer.removeEventListener('scroll', noscroll);
@@ -196,19 +209,19 @@
 			zoomer.style.WebkitTransform = 'translate3d(0,0,0) scale3d(1,1,1)';
 			zoomer.style.transform = 'translate3d(0,0,0) scale3d(1,1,1)';
 			/* fix for safari flickering */
-			
+
 			// scale up - behind the scenes - the item again (without transition)
 			applyTransforms(zoomer);
-			
+
 			// animate/scale down the item
-			setTimeout(function() {	
+			setTimeout(function () {
 				classie.remove(zoomer, 'zoomer--notrans');
 				classie.remove(zoomer, 'zoomer--active');
 				zoomer.style.WebkitTransform = 'translate3d(0,0,0) scale3d(1,1,1)';
 				zoomer.style.transform = 'translate3d(0,0,0) scale3d(1,1,1)';
 			}, 25);
 
-			if( bodyScale ) {
+			if (bodyScale) {
 				dynamics.css(bodyEl, { scale: bodyScale });
 				dynamics.animate(bodyEl, { scale: 1 }, {
 					type: dynamics.easeInOut,
@@ -223,18 +236,18 @@
 	// applies the necessary transform value to scale the item up
 	function applyTransforms(el, nobodyscale) {
 		// zoomer area and scale value
-		var zoomerArea = el.querySelector('.zoomer__area'), 
-			zoomerAreaSize = {width: zoomerArea.offsetWidth, height: zoomerArea.offsetHeight},
+		var zoomerArea = el.querySelector('.zoomer__area'),
+			zoomerAreaSize = { width: zoomerArea.offsetWidth, height: zoomerArea.offsetHeight },
 			zoomerOffset = zoomerArea.getBoundingClientRect(),
-			scaleVal = zoomerAreaSize.width/zoomerAreaSize.height < win.width/win.height ? win.width/zoomerAreaSize.width : win.height/zoomerAreaSize.height;
+			scaleVal = zoomerAreaSize.width / zoomerAreaSize.height < win.width / win.height ? win.width / zoomerAreaSize.width : win.height / zoomerAreaSize.height;
 
-		if( bodyScale && !nobodyscale ) {
-			scaleVal /= bodyScale; 
+		if (bodyScale && !nobodyscale) {
+			scaleVal /= bodyScale;
 		}
 
 		// apply transform
-		el.style.WebkitTransform = 'translate3d(' + Number(win.width/2 - (zoomerOffset.left+zoomerAreaSize.width/2)) + 'px,' + Number(win.height/2 - (zoomerOffset.top+zoomerAreaSize.height/2)) + 'px,0) scale3d(' + scaleVal + ',' + scaleVal + ',1)';
-		el.style.transform = 'translate3d(' + Number(win.width/2 - (zoomerOffset.left+zoomerAreaSize.width/2)) + 'px,' + Number(win.height/2 - (zoomerOffset.top+zoomerAreaSize.height/2)) + 'px,0) scale3d(' + scaleVal + ',' + scaleVal + ',1)';
+		el.style.WebkitTransform = 'translate3d(' + Number(win.width / 2 - (zoomerOffset.left + zoomerAreaSize.width / 2)) + 'px,' + Number(win.height / 2 - (zoomerOffset.top + zoomerAreaSize.height / 2)) + 'px,0) scale3d(' + scaleVal + ',' + scaleVal + ',1)';
+		el.style.transform = 'translate3d(' + Number(win.width / 2 - (zoomerOffset.left + zoomerAreaSize.width / 2)) + 'px,' + Number(win.height / 2 - (zoomerOffset.top + zoomerAreaSize.height / 2)) + 'px,0) scale3d(' + scaleVal + ',' + scaleVal + ',1)';
 	}
 
 	// navigate the slider
@@ -244,23 +257,23 @@
 			currentTitleEl = itemCurrent.querySelector('.slide__title');
 
 		// update new current value
-		if( dir === 'right' ) {
-			current = current < itemsTotal-1 ? current + 1 : 0;
+		if (dir === 'right') {
+			current = current < itemsTotal - 1 ? current + 1 : 0;
 		}
 		else {
-			current = current > 0 ? current - 1 : itemsTotal-1;
+			current = current > 0 ? current - 1 : itemsTotal - 1;
 		}
 
 		var itemNext = items[current],
 			nextEl = itemNext.querySelector('.slide__mover'),
 			nextTitleEl = itemNext.querySelector('.slide__title');
-		
+
 		// animate the current element out
-		dynamics.animate(currentEl, { opacity: 0, translateX: dir === 'right' ? -1*currentEl.offsetWidth/2 : currentEl.offsetWidth/2, rotateZ: dir === 'right' ? -10 : 10 }, {
+		dynamics.animate(currentEl, { opacity: 0, translateX: dir === 'right' ? -1 * currentEl.offsetWidth / 2 : currentEl.offsetWidth / 2, rotateZ: dir === 'right' ? -10 : 10 }, {
 			type: dynamics.spring,
 			duration: 2000,
 			friction: 600,
-			complete: function() {
+			complete: function () {
 				dynamics.css(itemCurrent, { opacity: 0, visibility: 'hidden' });
 			}
 		});
@@ -268,21 +281,21 @@
 		// animate the current title out
 		dynamics.animate(currentTitleEl, { translateX: dir === 'right' ? -250 : 250, opacity: 0 }, {
 			type: dynamics.bezier,
-			points: [{"x":0,"y":0,"cp":[{"x":0.2,"y":1}]},{"x":1,"y":1,"cp":[{"x":0.3,"y":1}]}],
+			points: [{ "x": 0, "y": 0, "cp": [{ "x": 0.2, "y": 1 }] }, { "x": 1, "y": 1, "cp": [{ "x": 0.3, "y": 1 }] }],
 			duration: 450
 		});
 
 		// set the right properties for the next element to come in
 		dynamics.css(itemNext, { opacity: 1, visibility: 'visible' });
-		dynamics.css(nextEl, { opacity: 0, translateX: dir === 'right' ? nextEl.offsetWidth/2 : -1*nextEl.offsetWidth/2, rotateZ: dir === 'right' ? 10 : -10 });
+		dynamics.css(nextEl, { opacity: 0, translateX: dir === 'right' ? nextEl.offsetWidth / 2 : -1 * nextEl.offsetWidth / 2, rotateZ: dir === 'right' ? 10 : -10 });
 
 		// animate the next element in
 		dynamics.animate(nextEl, { opacity: 1, translateX: 0 }, {
 			type: dynamics.spring,
 			duration: 2000,
 			friction: 600,
-			complete: function() {
-				items.forEach(function(item) { classie.remove(item, 'slide--current'); });
+			complete: function () {
+				items.forEach(function (item) { classie.remove(item, 'slide--current'); });
 				classie.add(itemNext, 'slide--current');
 			}
 		});
@@ -292,14 +305,14 @@
 		// animate the next title in
 		dynamics.animate(nextTitleEl, { translateX: 0, opacity: 1 }, {
 			type: dynamics.bezier,
-			points: [{"x":0,"y":0,"cp":[{"x":0.2,"y":1}]},{"x":1,"y":1,"cp":[{"x":0.3,"y":1}]}],
+			points: [{ "x": 0, "y": 0, "cp": [{ "x": 0.2, "y": 1 }] }, { "x": 1, "y": 1, "cp": [{ "x": 0.3, "y": 1 }] }],
 			duration: 650
 		});
 	}
 
 	// disallow scrolling (on the scrollContainer)
 	function noscroll() {
-		if(!lockScroll) {
+		if (!lockScroll) {
 			lockScroll = true;
 			xscroll = scrollContainer.scrollLeft;
 			yscroll = scrollContainer.scrollTop;
