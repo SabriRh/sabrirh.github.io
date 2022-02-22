6cc008d00de93a58e3cb63c50361635dbe1147a0
Sabri RHAIMIA <ZB5853@engie.com>
2022-02-22 17:28:47 +0100

    fix close content && add project titles and description

--- js/script.js
+++ js/script.js
@@ -13,8 +13,10 @@
 	const total = navdemos.length;
 	const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
 	const navigate = (linkEl) => {
-		document.body.classList.remove('render');
-		document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+		if (linkEl && linkEl.href) {
+			document.body.classList.remove('render');
+			document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+		}
 	};
 	navdemos.forEach(link => link.addEventListener('click', (ev) => {
 		ev.preventDefault();
